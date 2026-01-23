de0b3845b2464b1dba459f280b201beab3c3ac88
silvaaa2 <dr947695@gmail.com>
2026-01-23 15:51:49 +0000

    Refactor showCategory function and add loginAdmin

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-function showCategory(categoryId) {
-    const categories = document.querySelectorAll('.category');
-    
-    categories.forEach(category => {
-        category.style.display = 'none';
+function showCategory(id) {
+    document.querySelectorAll('.category').forEach(section => {
+        section.style.display = 'none';
     });
 
-    document.getElementById(categoryId).style.display = 'block';
+    const section = document.getElementById(id);
+    section.style.display = 'block';
+}
+
+/* ZONE ADMIN */
+function loginAdmin() {
+    const user = document.getElementById("adminUser").value;
+    const pass = document.getElementById("adminPass").value;
+
+    if (user === "admin" && pass === "1234") {
+        document.getElementById("adminLogin").style.display = "none";
+        document.getElementById("adminPanel").style.display = "block";
+    } else {
+        alert("Accès refusé ❌");
+    }
 }
