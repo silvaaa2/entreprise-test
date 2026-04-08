63e0509624c419ff165671429a5779fa9cc512af
silvaaa2 <dr947695@gmail.com>
2026-04-08 11:06:49 +0100

    Refactor main.js to include authentication functions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-console.log("main.js chargé");
+import { login, loginWithGoogle, resetPassword, logout, initAuthListener } from "./auth.js";
+import { showSection } from "./ui.js";
+
+window.login = login;
+window.loginWithGoogle = loginWithGoogle;
+window.resetPassword = resetPassword;
+window.logout = logout;
+window.showSection = showSection;
+
+initAuthListener();
