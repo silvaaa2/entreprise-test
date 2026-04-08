78826e0196d411ad856400cf01f479283b81e051
silvaaa2 <dr947695@gmail.com>
2026-04-08 11:16:52 +0100

    Refactor imports and add theme functions

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
-import { login, loginWithGoogle, resetPassword, logout, initAuthListener } from "./auth.js";
-import { showSection } from "./ui.js";
+import {
+  login,
+  loginWithGoogle,
+  resetPassword,
+  logout,
+  initAuthListener
+} from "./auth.js";
 
+import {
+  showSection,
+  toggleTheme,
+  initTheme
+} from "./dashboard.js";
+
+// rendre accessibles au HTML (onclick)
 window.login = login;
 window.loginWithGoogle = loginWithGoogle;
 window.resetPassword = resetPassword;
 window.logout = logout;
 window.showSection = showSection;
+window.toggleTheme = toggleTheme;
 
+// init
 initAuthListener();
+initTheme();
